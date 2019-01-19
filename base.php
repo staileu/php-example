00cc2b872a98c9e41de270b89c97bd24fb0a4d59
Le_Futuriste <spamfree@matthieubessat.fr>
2019-01-19 23:28:25 +0100

    fix: another facepalm with public exposed secret on github

--- base.php
+++ base.php
@@ -4,8 +4,8 @@ require 'vendor/autoload.php';
 
 // CHANGE ME!
 // CHANGE HERE THE APP ID, APP SECRET AND THE REDIRECTION URI
-$appId = "ZIR7blbDfmFjavbl";
-$appSecret = "MTMuEMyeukICIkfWdmVpn6P9UJMwrgZ0";
+$appId = "YOUR_APP_ID";
+$appSecret = "YOUR_APP_SECRET";
 $redirectionUri = "http://localhost:80/execute.php";
 
 // start the session
